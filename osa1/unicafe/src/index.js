8ca407bf97f5b7b1c59fb7459c35927ce8ce92cb
Niko Häppölä <niko.happola@gmail.com>
2020-01-24 15:45:33 +0200

    tehtävät 1.7 & 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -13,6 +13,24 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const feedbacks = [props.good, props.neutral, props.bad]
+  const total = feedbacks[0] + feedbacks[1] + feedbacks[2]
+  const average = total / 3
+  const positives = feedbacks[0] / total * 100
+
+  return (
+  <div>
+    <p>Good: {feedbacks[0]}</p>
+    <p>Neutral: {feedbacks[1]}</p>
+    <p>Bad: {feedbacks[2] }</p>
+    <p>All: {total}</p>
+    <p>Average: {average}</p>
+    <p>Positive: {positives} %</p>
+  </div>
+  )
+}
+
 const App = (props) => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -34,9 +52,7 @@ const App = (props) => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
       <Header header="Statistics" />
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

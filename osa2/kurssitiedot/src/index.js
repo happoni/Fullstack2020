5bf52a4b97886739e85a855cc1fd2e09241c7290
Niko Häppölä <niko.happola@gmail.com>
2020-01-31 14:03:09 +0200

    tehtävät 2.1, 2.2 ja 2.3

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Course = ({course}) => {
+
+  const Header = () => {
+    return (
+        <div>
+            <h1>{course.name}</h1>
+        </div>
+    )
+  }
+
+  const Content = () => {    
+    
+    return (
+      <div>
+        {course.parts.map(part =>
+          <Part key={part.id} id={part.id} name={part.name} exercises={part.exercises} />          
+          )}                            
+      </div>)
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p key={props.id}>
+          {props.name} {props.exercises}
+        </p>
+      </div>)
+  }
+
+  const Total = () => {
+    
+    const exercisesTotal = course.parts.map(part =>
+      part.exercises).reduce(
+      ( accumulator, currentValue ) => accumulator + currentValue)    
+    
+    return (
+        <div>
+          <b>
+            Total number of exercises {exercisesTotal}
+          </b>
+        </div>
+    )
+  }
+
+  return (
+    <div>
+    <Header />
+    <Content />
+    <Total />
+    </div>
+  )
+}
+
+const App = () => {
+    const course = {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },    
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+       },    
+       {
+         name: "Testing against failures",
+         exercises: 5,
+         id: 4
+       }
+      ]
+    }
+
+    return (
+        <div>
+          <Course course={course} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

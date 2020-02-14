82c422182d5da7c5b4dee99a75c80dc71512de33
Niko Häppölä <niko.happola@gmail.com>
2020-02-14 12:28:35 +0200

    eom

---
+++ osa4/blogilistasovellus/app.js
@@ -0,0 +1,48 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+// muutamia muita tulossa
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to ', config.MONGODB_URI)
+
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+
+
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app

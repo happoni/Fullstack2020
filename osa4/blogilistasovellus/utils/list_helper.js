45661a60d3808339081715ff318c01a8e0226c35
Niko Häppölä <niko.happola@gmail.com>
2020-02-14 17:46:03 +0200

    tehtävä 4.4

--- osa4/blogilistasovellus/utils/list_helper.js
+++ osa4/blogilistasovellus/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const initialValue = 0
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((acc, cur) => acc + cur.likes, initialValue)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }

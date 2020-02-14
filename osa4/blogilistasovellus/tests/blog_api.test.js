73883bde21feba1671561fd3d5236db97b3b2ad0
Niko Häppölä <niko.happola@gmail.com>
2020-02-14 19:23:24 +0200

    tehtävä 4.8

---
+++ osa4/blogilistasovellus/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('basic api returns', () => {
+  test('blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+  })
+
+  test('correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})

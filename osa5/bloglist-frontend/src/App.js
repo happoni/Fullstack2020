e5ad7ab62f891e77cb1438fb9d55af074aba6de6
Niko Häppölä <niko.happola@gmail.com>
2020-02-21 15:38:22 +0200

    tehtävä 5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -6,6 +6,10 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -57,6 +61,25 @@ const App = () => {
     setUser(null)
   }
 
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewBlog('')
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+      })
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -79,6 +102,54 @@ const App = () => {
       </div>
       <button type="submit">login</button>
     </form>      
+  )
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const blogForm = () => (
+    <div>
+      <h4>Add a new blog</h4>
+      <form onSubmit={addBlog}>
+        <div>
+            Title: 
+            <input
+              type="text"
+              value={newTitle}
+              name="title"
+              onChange={handleTitleChange}
+            />
+          <br></br>
+            Author:
+            <input
+              type="text"
+              value={newAuthor}
+              name="author"
+              onChange={handleAuthorChange}
+            />
+          <br></br>
+            Url:
+            <input
+              type="text"
+              value={newUrl}
+              name="url"
+              onChange={handleUrlChange}
+            />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </div>
   )
 
   if (user === null) {
@@ -105,6 +176,7 @@ const App = () => {
         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+        {blogForm()}
     </div>
   )
 }

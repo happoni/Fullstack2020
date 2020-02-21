a037271b1bfbb4de80b6fb719dae3d20348025c3
Niko Häppölä <niko.happola@gmail.com>
2020-02-21 17:18:28 +0200

    tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Blog = ({ blog }) => {
-  return (
-    <li className='blog'>
-      {blog.title} - {blog.author} 
-    </li>
-  )
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [minimized, setMinimized] = useState(true)
+
+  const toggleMinimize = () => {
+    setMinimized(!minimized)
+  }
+  
+  if (minimized) {
+    return (
+      <div style={blogStyle}>
+          {blog.title} - {blog.author} 
+          <button onClick={() => toggleMinimize()}>Show</button>
+      </div>
+    )    
+  } else {
+    return (
+      <div style={blogStyle}>
+          {blog.title} - {blog.author}
+          <br></br>
+          {blog.url}
+          <br></br>
+          Likes: {blog.likes}
+          <br></br>
+          User: {blog.user.name}
+          <button onClick={() => toggleMinimize()}>Hide</button>      
+      </div>
+    )
+  }
 }
 
 export default Blog
